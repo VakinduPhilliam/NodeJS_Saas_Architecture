cf03909277365e31a65f353497fd93de8ac44246
Vakindu Philliam <Pvakindu@gmail.com>
2019-07-01 20:22:25 +0300

    Add files via upload

---
+++ client/app/main/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+
+angular.module('saasApp')
+.config(function ($stateProvider) {
+    
+
+$stateProvider
+.state('main', {
+ url: '/',
+ templateUrl: 'app/main/main.html',
+ controller: 'MainCtrl'
+ });
+  });
