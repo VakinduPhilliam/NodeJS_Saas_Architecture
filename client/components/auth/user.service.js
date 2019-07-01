2f1b1bf752e06f514b087354722114f3b6c68c3d
Vakindu Philliam <Pvakindu@gmail.com>
2019-07-01 20:27:04 +0300

    Add files via upload

---
+++ client/components/auth/user.service.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+
+angular.module('saasApp')
+.factory('User', function ($resource) {
+    
+
+return $resource('/api/users/:id/:controller', {
+ id: '@_id'
+ },
+    
+
+{
+      
+
+changePassword: {
+ 
+
+method: 'PUT',
+        
+
+params: {
+ controller:'password'
+ }
+ },
+      
+
+get: {
+        
+
+method: 'GET',
+        
+
+params: {
+  id:'me'
+  }
+      
+      }
+	  
+
+   });
+  
+
+});
